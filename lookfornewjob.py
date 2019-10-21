693d34974960377dd4428646ffe50f15a267aa9f
robertcanare <root@localhost.localdomain>
2019-10-22 07:43:06 +0800

    addede dev

--- lookfornewjob.py
+++ lookfornewjob.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3                                
 #Just a simple script to automate my job application
-
+hadjskjshdksah
 #test123
 import smtplib
 from email.mime.multipart import MIMEMultipart
